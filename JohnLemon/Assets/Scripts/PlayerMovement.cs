f5d04a7b158a18c489ae8a37adf42aa36914eb54
지원 <92582758+jasumine@users.noreply.github.com>
2023-11-07 16:56:03 +0900

    1107

--- JohnLemon/Assets/Scripts/PlayerMovement.cs
+++ JohnLemon/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float turnSpeed = 20f;
+
+    
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
 
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -26,5 +33,31 @@ public class PlayerMovement : MonoBehaviour
         // 움직인다면 0이 아니라서 fale지만 !를 썻기때문에 true가 된다.
         // input은 움직일경우 true가 된다.
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+        if(isWalking)
+        {
+            if(!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
